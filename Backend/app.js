a390cbb4fa7c04951be055f8d91aadeba59f48c1
LBPilots51 <libbyblair13@gmail.com>
2023-01-23 15:08:33 -0600

    Making content page links. Specifically GetStarted

--- Backend/app.js
+++ Backend/app.js
@@ -1,28 +1,23 @@
 const express = require("express");
 //const db = require("./db");
-const Sequelize = require('sequelize');
-const dbConnection = require('./config/dbConnection').database;
+const Sequelize = require("sequelize");
+const dbConnection = require("./config/dbConnection").database;
 const dotenv = require("dotenv");
-const path = require('path');
+const path = require("path");
 const { appendFile } = require("fs");
 const { handlebars } = require("hbs");
-const hbs = require('express-handlebars');
-
-
-
-
+const hbs = require("express-handlebars");
 
 //Save sensitive information
-dotenv.config({ path: './.env'});
+dotenv.config({ path: "./.env" });
 
 //server
 const app = express();
 
-
-const publicDirectory =path.join(__dirname,"public");
+const publicDirectory = path.join(__dirname, "public");
 
 //So we can grab data from any form
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 //Make sure values come in as Json Objects
 app.use(express.json());
 
@@ -30,31 +25,43 @@ app.use(express.json());
 app.use(express.static(publicDirectory));
 
 // To use boostrap both offline and online
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
-
-
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/jquery/dist"))
+);
 
 // set the view engine to ejs
 // use res.render to load up an ejs view file
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 //Layouts for the frontend using main.html
-app.engine( 'hbs', hbs.engine( {
-    extname: 'hbs',
-    defaultView: 'default',
-    layoutsDir: __dirname + '/views/layouts/'
-}));
-
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: "hbs",
+    defaultView: "default",
+    layoutsDir: __dirname + "/views/layouts/",
+  })
+);
 
 //Database Connection
-dbConnection.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
- }).catch((error) => {
-    console.error('Unable to connect to the database: ', error);
- });
- 
+dbConnection
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
+
 /*db.connect((error)=> {
     if(error){
         console.log(error)
@@ -64,9 +71,10 @@ dbConnection.authenticate().then(() => {
 }) */
 
 //Define Routes here
-app.use('/', require('./routes/pages'))
-app.use('/auth', require('./routes/auth'))
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
+app.use("/task", require("./routes/task"));
 
 app.listen(5000, () => {
-    console.log("server started on port 5000")
-})
+  console.log("server started on port 5000");
+});
