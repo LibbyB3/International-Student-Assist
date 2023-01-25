fe05261aa2d858adf144ad3890cdfc1cc56c3f8f
marvinbattley <marvinbattley@hotmail.com>
2023-01-25 14:06:02 -0600

    Created active links to task pages

--- Backend/routes/task.js
+++ Backend/routes/task.js
@@ -16,7 +16,79 @@ router.get("/", validateToken, (req, res) => {
 
 router.get("/get-started", validateToken, (req, res) => {
   if (res.locals.authenticated) {
-    res.render("getStarted");
+    res.render("get-started");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/research-schools", validateToken, (req, res) => {
+  if (res.locals.authenticated) {
+    res.render("research");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/apply", validateToken, (req, res) => {
+  if (res.locals.authenticated) {
+    res.render("apply");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/submit-transcripts", validateToken, (req, res) => {
+  if (res.locals.authenticated) {
+    res.render("transcripts");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/english-proficiency", validateToken, (req, res) => {
+  if (res.locals.authenticated) {
+    res.render("eng-proficiency");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/testing-and-scores", validateToken, (req, res) => {
+  if (res.locals.authenticated) {
+    res.render("testing");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/accepted", validateToken, (req, res) => {
+  if (res.locals.authenticated) {
+    res.render("accepted");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/i20-visa-and-fees", validateToken, (req, res) => {
+  if (res.locals.authenticated) {
+    res.render("visa");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/relocate", validateToken, (req, res) => {
+  if (res.locals.authenticated) {
+    res.render("relocate");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/additional-tips", validateToken, (req, res) => {
+  if (res.locals.authenticated) {
+    res.render("tips");
   } else {
     res.redirect("/login");
   }
