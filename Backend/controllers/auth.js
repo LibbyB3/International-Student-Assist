7b2a6b878fcc659c2d94e9c249e2611e32427023
marvinbattley <marvinbattley@hotmail.com>
2023-02-08 14:58:57 -0600

    Populated Testing, Transcripts, Eng-Proficiency, and Accepted Pages

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -4,56 +4,56 @@ const bcrypt = require('bcryptjs')
 const dbConnection = require('../config/dbConnection').database;
 const User = require("../model/user")
 const cookieParser = require("cookie-parser");
-const {createTokens , validateToken} = require('../JWT')
+const { createTokens, validateToken } = require('../JWT')
 
 
 //Save sensitive information
-dotenv.config({ path: './.env'});
+dotenv.config({ path: './.env' });
 
 
-exports.register = async (req,res) => {
+exports.register = async (req, res) => {
 
-    var name =req.body.name;
+    var name = req.body.name;
     var email = req.body.email;
     var password = req.body.password;
     var passwordConfirm = req.body.passwordConfirm;
 
     try {
-        const user = await User.findOne({where: {email : email}})
+        const user = await User.findOne({ where: { email: email } })
         if (!user) {
-            if( password !== passwordConfirm){
+            if (password !== passwordConfirm) {
                 return res.render('register', {
                     message: 'Passwords do not match'
                 })
-            }else{
-            const hashedPassword = await bcrypt.hash(password, 8);
-    
-            // Create a new user with the provided name, email, and hashed password
-            const newUser = User.create({
-                name,
-                email,
-                password: hashedPassword,
-                role:"user"
-            }).then((newUser) => {
-                //Write code for authenticated users, JWT 
-                const accessToken = createTokens(newUser)
+            } else {
+                const hashedPassword = await bcrypt.hash(password, 8);
 
-                res.cookie("access-token", accessToken, {
-                    httpOnly:true,
-                    maxAge: 12*60*60*1000
-                });
+                // Create a new user with the provided name, email, and hashed password
+                const newUser = User.create({
+                    name,
+                    email,
+                    password: hashedPassword,
+                    role: "user"
+                }).then((newUser) => {
+                    //Write code for authenticated users, JWT 
+                    const accessToken = createTokens(newUser)
 
-                return res.render('register', {
-                    message: 'User Registered'
+                    res.cookie("access-token", accessToken, {
+                        httpOnly: true,
+                        maxAge: 12 * 60 * 60 * 1000
+                    });
+
+                    return res.render('register', {
+                        message: 'User Registered'
+                    });
                 });
-            }); 
-        }
-            
-        }else if(user){
+            }
+
+        } else if (user) {
             return res.render('register', {
                 message: 'User already Exits, please Login'
             })
-        } 
+        }
     } catch (error) {
         console.log(error)
     }
@@ -61,7 +61,7 @@ exports.register = async (req,res) => {
 
 
 
-exports.login = async (req,res) => {
+exports.login = async (req, res) => {
     var { email, password } = req.body
     // Check if username and password is provided
     if (!email || !password) {
@@ -72,7 +72,7 @@ exports.login = async (req,res) => {
 
     try {
         const user = await User.findOne({ email, password })
-        
+
         if (!user) {
             return res.render('login', {
                 message: 'User not Found, Please Register'
@@ -80,30 +80,30 @@ exports.login = async (req,res) => {
         } else {
             //Compare both hashed password
             const dbpassword = user.password
-            bcrypt.compare(password,dbpassword).then((match) =>{
+            bcrypt.compare(password, dbpassword).then((match) => {
                 //return incorrect password if it doesnt match
-                if(!match){
+                if (!match) {
                     return res.render('login', {
                         message: 'Incorrect Password'
                     });
-                //return loggin if it matches 
-                }else{
+                    //return loggin if it matches 
+                } else {
 
                     //Write code for authenticated users, JWT 
                     const accessToken = createTokens(user)
 
                     res.cookie("access-token", accessToken, {
-                        httpOnly:true,
-                        maxAge: 12*60*60*1000
+                        httpOnly: true,
+                        maxAge: 12 * 60 * 60 * 1000
                     });
 
-                    
+
                     return res.redirect("/");
                 }
             })
         }
-        } catch (error) {
-            console.log(error)
-        }
-    
+    } catch (error) {
+        console.log(error)
+    }
+
 }
